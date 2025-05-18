8150f9753fe6743c52e11d3ec2cf8eec2ab71de5
tsohim <114642275+tsoihim@users.noreply.github.com>
2025-05-18 23:11:12 +0900

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "@Himnote",
+    pageTitle: "@HimNote",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
