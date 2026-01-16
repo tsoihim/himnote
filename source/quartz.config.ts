87e4f43bf29f227b7ffd1456c68d3c5223a5f575
tsohim <114642275+tsoihim@users.noreply.github.com>
2026-01-16 10:21:13 +0900

    Change baseUrl from quartz.jzhao.xyz to tsoihim.github.io/himnote

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       tagId: "G-4SYZD53LPN",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "tsoihim.github.io/himnote",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
